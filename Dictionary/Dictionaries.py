7aa4a982fc75f9d90287bd730728d54d4d722b95
sabsabrina <sabrinamanurung92@gmail.com>
2021-09-05 11:28:16 +0700

    Dictionary App

---
+++ Dictionary/Dictionaries.py
@@ -0,0 +1,5 @@
+MyDict = {1:'one', 2:'two', 3:'three'}
+print("My Dict : ", MyDict)
+print("My data structure type : ", type(MyDict))
+print("My Dict Key : ", MyDict.keys())
+print("My Dict Value : ", MyDict.values())
